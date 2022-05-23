aed0ba304da77ee83d059139336c51bc6bd1652f
Ashoka Chandra Wardhan <ashoka.w@swiggy.in>
2022-05-23 14:20:00 +0530

    test: codeql breaking

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const MINIMUM_QUERY_STRING_LENGTH = 4;
 const query = req => {
     const params = req.query;
-    if (
-        params.query === undefined ||
-        typeof params.query !== "string" ||
-        params.query.length < MINIMUM_QUERY_STRING_LENGTH
-    ) {
+    if (params.query === undefined || params.query.length < MINIMUM_QUERY_STRING_LENGTH) {
         return 1;
     }
 };
